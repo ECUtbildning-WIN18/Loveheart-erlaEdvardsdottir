7714e7f1b9853e663985ef1c0bd7737267a8fcf9
Erla Edvadsdottir <erlama85@gmail.com>
2018-10-04 09:27:49 +0200

    Add project files.

---
+++ Loveheart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loveheart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
